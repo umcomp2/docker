a93db3e0dbf175d78ff3b87af5a8ed5b4b5ee28c
carlost <carlos.taffe@gmail.com>
2020-10-28 21:57:35 -0300

    a basic load balancer

---
+++ load-balancing/app1/app1.py
@@ -0,0 +1,16 @@
+from flask import request, Flask
+import json
+
+
+app1 = Flask(__name__)
+
+
+@app1.route('/')
+def hello_world():
+    return 'hola mundo1, aca umcompu :) '
+
+
+if __name__ == '__main__':
+   app1.run(debug=True, host='0.0.0.0')
+
+
